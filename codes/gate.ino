62b2f2c61fca5447c83065084cbfb41410ae20a6
Aditya Gupta <118872081+Aditya2004-hue@users.noreply.github.com>
2023-09-17 20:59:55 +0530

    Create gate.ino

---
+++ codes/gate.ino
@@ -0,0 +1,16 @@
+int A,B,C;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(2,INPUT);
+  pinMode(3,INPUT);
+  pinMode(5,OUTPUT);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+ A = digitalRead(2);
+ B = digitalRead(3);
+ C = (!B);
+digitalWrite(5,C);
+}
