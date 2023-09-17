dd894e5294325f554431645428bd59ad430be7bb
Aditya Gupta <118872081+Aditya2004-hue@users.noreply.github.com>
2023-09-17 20:43:27 +0530

    Update codes.ino

--- codes.ino
+++ codes.ino
@@ -3,16 +3,14 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(2,INPUT);
   pinMode(3,INPUT);
-  pinMode(4,INPUT);
   pinMode(5,OUTPUT);
 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
- P = digitalRead(2);
- Q = digitalRead(3);
- R = digitalRead(4);
- A = (P && !Q);
-digitalWrite(5,A);
+ A = digitalRead(2);
+ B = digitalRead(3);
+ C = (!B);
+digitalWrite(5,C);
 }
